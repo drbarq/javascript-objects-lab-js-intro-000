83d835b665b7b590e14745ba503c3de47e82c6c1
Joe Tustin <j.tustin@gmail.com>
2019-02-16 22:18:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var recipes = {"key" : "value"};
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object.key="value";
+}
+
+
+
+
+
+/*
 function updateObjectWithKeyAndValue(object, key, value) {
   object.key="value"; {
   return object.assign({}, object, {[key]: value});
   }
 }
+*/
