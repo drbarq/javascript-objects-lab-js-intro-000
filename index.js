43eb32484ef6819b43b607aaefcf04748a759a83
Joe Tustin <j.tustin@gmail.com>
2019-02-16 22:25:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,5 @@ function destructivelyDeleteFromObjectByKey(object, key) {
 
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key]="value"; {
   return object.assign({}, object, {[key]: value});
-  }
 }
